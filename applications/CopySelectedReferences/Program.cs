0b7df91d4c305dfc13f4f71265709d4fee139005
J-M <jean-michel.perraud@csiro.au>
2013-12-10 10:34:04 +1100

    Add CopySelectedReferences (refactor the latest that was found in the TWB SVN repository)

---
+++ applications/CopySelectedReferences/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Reflection;
+using Utils.Lib.BuildSupport;
+
+namespace Utils.App.CopySelectedReferences
+{
+    /// <summary>
+    /// A program that was designed to find/copy the subset of assemblies and their dependencies from one or more folders.
+    /// </summary>
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            if (args.Length < 4 || args.Contains("-h"))
+            {
+                Console.WriteLine(@"Copy one or more assemblies and their dependencies to a destination folder");
+                Console.WriteLine(@"CopySelectedReferences directory fileWildCards copyto findRefDir1 [findRefDir2 ...]");
+                return;
+            }
+            string directory = args[0];
+            string fileWildCards = args[1];
+            string copyto = args[2];
+            var findRefDir = new string[args.Length-3];
+            Array.Copy(args, 3, findRefDir, 0, findRefDir.Length); 
+
+            var files = Directory.GetFiles( directory, fileWildCards, SearchOption.TopDirectoryOnly );
+
+            var candidateRefFiles = AssemblyDiscover.GetCandidateRefFiles(directory, findRefDir);
+            candidateRefFiles = AssemblyDiscover.FilterOut(candidateRefFiles, "Direct"); // some DirectX assembly caused problems with TIME
+            var candidateAsmFiles = AssemblyDiscover.GetCandidateAssemblies(candidateRefFiles);
+            var toCopy = AssemblyDiscover.GetFilesToCopy(files, candidateAsmFiles);
+            var pdbFiles = AssemblyDiscover.GetPdbFiles(toCopy);
+
+            AssemblyDiscover.CopyFiles(toCopy, copyto);
+            AssemblyDiscover.CopyFiles(pdbFiles, copyto);
+        }
+    }
+}
---
+++ libraries/BuildSupport/AssemblyDiscover.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Utils.Lib.BuildSupport
+{
+    public class AssemblyDiscover
+    {
+        public static void CopyFiles(IEnumerable<string> toCopy, string destinationDir)
+        {
+            foreach (var file in toCopy)
+            {
+                if (File.Exists(file))
+                    File.Copy(file, Path.Combine(destinationDir, Path.GetFileName(file)), true);
+            }
+        }
+
+        public static string[] GetPdbFiles(List<string> toCopy)
+        {
+            var pdbFiles = Array.ConvertAll(toCopy.ToArray(),
+                                            (x =>
+                                             Path.Combine(Path.GetDirectoryName(x), Path.GetFileNameWithoutExtension(x) + ".pdb")));
+            return pdbFiles;
+        }
+
+        public static List<string> GetFilesToCopy(string[] files, Dictionary<string, Assembly> candidateAsmFiles)
+        {
+            var toCopy = new List<string>(files);
+            //            var allFiles = new List<string>( );
+            foreach (var file in files)
+            {
+                AddReferencedAssemblies(file, candidateAsmFiles, toCopy);
+            }
+            return toCopy;
+        }
+
+        public static Dictionary<string, Assembly> GetCandidateAssemblies(string[] candidateRefFiles)
+        {
+            //var assemblies = Array.ConvertAll( candidateRefFiles, ( x => Assembly.LoadFrom( x ) ) );
+            var candidateAsmFiles = new Dictionary<string, Assembly>();
+            for (int i = 0; i < candidateRefFiles.Length; i++)
+            {
+                if (!candidateAsmFiles.ContainsKey(candidateRefFiles[i]))
+                {
+                    try
+                    {
+                        candidateAsmFiles.Add(candidateRefFiles[i], Assembly.LoadFrom(candidateRefFiles[i]));
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+            return candidateAsmFiles;
+        }
+
+        public static string[] GetCandidateRefFiles(string directory, params string[] findRefDir)
+        {
+            var candidateRefFiles = Directory.GetFiles(directory, "*.dll", SearchOption.TopDirectoryOnly).ToList();
+            for (int i = 0; i < findRefDir.Length; i++)
+                candidateRefFiles.AddRange(Directory.GetFiles(findRefDir[i], "*.dll", SearchOption.TopDirectoryOnly));
+
+            return candidateRefFiles.ToArray();
+        }
+
+        public static string[] FilterOut(string[] filepaths, params string[] stringsToFilterOut)
+        {
+            string[] res = filepaths;
+            for (int j = 0; j < stringsToFilterOut.Length; j++)
+            {
+                res = oneFilterOut(res, stringsToFilterOut[j]);
+            }
+            return res.ToArray();
+        }
+
+        private static string[] oneFilterOut(string[] filepaths, string stringToFilterOut)
+        {
+            return filepaths.Where((x => (x.Contains(stringToFilterOut) == false))).ToArray();
+        }
+
+        public static void AddReferencedAssemblies(string file, Dictionary<string, Assembly> candidateAsmFiles, List<string> toCopy)
+        {
+            try
+            {
+                var assembly = Assembly.LoadFrom(file);
+                var referencedAssemblies = assembly.GetReferencedAssemblies();
+                foreach (var refAss in referencedAssemblies)
+                {
+                    var match = candidateAsmFiles.Where((x => x.Value.GetName().Name == refAss.Name));
+                    var matchingFilename = match.FirstOrDefault();
+                    if (matchingFilename.Key != null && !toCopy.Contains(matchingFilename.Key))
+                    {
+                        toCopy.Add(matchingFilename.Key);
+                        AddReferencedAssemblies(matchingFilename.Key, candidateAsmFiles, toCopy);
+                    }
+                }
+            }
+            catch
+            {
+                // Most likely, a native DLL.
+            }
+        }
+
+    }
+}
